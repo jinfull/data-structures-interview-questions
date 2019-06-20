693b505ec9efa7fb5e6efbf759fa9debe01d89fc
jinfull <jinfull@gmail.com>
2019-06-19 17:09:40 -0700

    Basic logic for balancedParens minus logic for other brackets

--- 01_stack_queue_interview_problems/lib/1_balanced_parens.js
+++ 01_stack_queue_interview_problems/lib/1_balanced_parens.js
@@ -76,7 +76,21 @@
 // Let's code!
 // -----------
 function balancedParens(str) {
+  let stack = [];
 
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(') {
+      stack.push('(');
+    } else if (str[i] === ')' && stack.length === 0) {
+      return false;
+    } else if (str[i] === ')') {
+      stack.pop();
+    }
+  }
+
+  if (stack.length === 0) return true;
+
+  return false;
 }
 
 exports.balancedParens = balancedParens;
